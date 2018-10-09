a0873dfc6a094c49dbc2109a8e08b87be6599d9d
Jackson Porter <jackson_porter@outlook.com>
2018-10-08 23:56:37 -0600

    Added basic shell script

---
+++ CanvasSubmissionUnpacker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+###########################################
+#                                         #
+#     ########  ###########  #        #   #
+#    #          #            #        #   #
+#   #           #            #        #   #
+#   #           ###########  #        #   #
+#   #                     #  #        #   #
+#    #                    #  #        #   #
+#     ########  ###########  ##########   #
+#                                         #
+###########################################
+# Canvas Submission Unpacker 
+# https://github.com/jacksonporter/canvassubmissionunpacker
+# Is NOT affiliated with Canvas by Instructure.
+
+# This script takes a compressed zip folder of submissions from Canvas by Instructure
+# unzips it, creates a new directory, creates a new folder for each student 
+# submission, renames the files to have the original name (part of the problem with
+# downloading all submissions from a single assingment in Canvas).
+
+# There are no guarantees from using this script. This is a Bash shell script, which uses
+# various Linux/Unix programs to complete the above described tasks. I do not take 
+# responsibility for the usage and output of this software. USE AT YOUR OWN RISK!
+# This script/program is lisened by the MIT License. Please see the GitHub repository
+# below and open the License file for more information.
+
+# This is a project based at: https://github.com/jacksonporter/canvassubmissionunpacker
+
+#functions:
+
+
+
+
+
+
+
+
+#main:
+printf "You are running the Canvas Submission Unpacker Bash Script."
+printf "This project is available at https://github.com/jacksonporter/canvassubmissionunpacker\n"
