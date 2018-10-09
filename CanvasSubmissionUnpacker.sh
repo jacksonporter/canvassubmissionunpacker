14daabb3d734377b3ecb5ccb1ec7abb39dfd619a
jacksonporter <jackson_porter@outlook.com>
2018-10-09 03:15:13 -0600

    Added support for command line args

--- CanvasSubmissionUnpacker.sh
+++ CanvasSubmissionUnpacker.sh
@@ -28,27 +28,36 @@
 
 # This is a project based at: https://github.com/jacksonporter/canvassubmissionunpacker
 
-# Global Variables
-COMPRESSEDSUBMISSIONS=""
-ASSINGMENTNAME="Default"
-
 # Functions:
 
+function setGlobalVariables() {
+    if [ -z "$COMPRESSEDSUBMISSIONS" ]
+    then
+        COMPRESSEDSUBMISSIONS=""
+    fi
+
+    if [[ -z "$ASSINGMENTNAME" ]]
+    then
+        ASSINGMENTNAME="DEFAULT"
+    fi
+}
+
+function printUsage() {
+    printf "USAGE: \nbash CanvasSubmissionUnPacker.sh\nOptions:"
+    printf "\n\t -a NameOfAssingment (Name of assingment associated with submission)"
+    printf "\n\t -u (Displayes usage of CanvasSubmissionUnpacker bash shell script)"
+    printf "\n"
+}
+
 function cleanUpCtrlC() {
-    cleanUp "Program killed by (CTRL + C)." 1
+    printf "\n\nProgram killed by (CTRL + C)"
+    cleanUp 1
 }
 
 # Cleans up the program by printing thank you message and an additional message if needed 
 # and clears the console.
 function cleanUp() {
-    clear # clear the console
-    printf "Closing Canvas Submission Unpacker. " # print closing message
-
-    if [[ "$1" != "" ]] && [[ "$1" != "0" ]] && [[ "$1" != "1" ]] #if an argument is provided, print it.
-    then
-        printf "\n\nMessage: $1\n"
-        shift
-    fi
+    printf "\n\n\nClosing Canvas Submission Unpacker.\n" # print closing message
 
     if [ $1 -eq 1 ]
     then
@@ -59,7 +68,7 @@ function cleanUp() {
 
             if [ input == "Y" ] || [ input == "y" ]
             then 
-                printf "Deleting all folders/files in created directory and deleting directory, $createdDir."
+                printf "Deleting all folders/files in created directory and deleting directory, $ASSINGMENTNAME."
                 rm -rf "./$ASSINGMENTNAME"
             else
                 printf "The $ASSINGMENTNAME directory was left behind.\n"
@@ -78,8 +87,8 @@ function setCompressedZipSubmissions() {
         then
             printf "\nGreat! I see your zip compressed folder.\n"
         else
-            cleanUp "Can't see compresssed zip folder. Try again: bash CanvasSubmissionUnpacker -s \"\\path\\to\\zip.zip\"" 1
-           printf "\nLooks like I can't see that compressed zip folder. Please try again. [Press (CTRL + C) to quit]: "
+            printf "\nCan't see compresssed zip folder. Try again: bash CanvasSubmissionUnpacker -s \"\\path\\to\\zip.zip\""
+            cleanUp 1
         fi
     else  # Grab the location of the submissions compressed zip folder from the user.
         printf "Please type in the name of the ZIP compressed folder with the submissions (don't forget the .zip at the end): "
@@ -108,7 +117,7 @@ function makeAssingmentDirectory() {
             printf "\nThere is already a directory with this name in this folder. I will write into this directory.\n"
         else
             mkdir "./$ASSINGMENTNAME"
-            printf "\nCreated diretory: $ASSINGMENTNAME."
+            printf "\nCreated directory: $ASSINGMENTNAME.\n"
         fi
     else  # Grab the location of the submissions compressed zip folder from the user.
         printf "Please type in the name of the assingment that goes with the submissions (Please, no spaces): "
@@ -119,7 +128,7 @@ function makeAssingmentDirectory() {
             printf "\nThere is already a directory with this name in this folder. I will write into this directory.\n"
         else
             mkdir "./$ASSINGMENTNAME"
-            printf "\nCreated diretory: $ASSINGMENTNAME."
+            printf "\nCreated directory: $ASSINGMENTNAME.\n"
         fi
     fi
 }
@@ -134,7 +143,73 @@ trap cleanUpCtrlC SIGINT
 printf "You are running the Canvas Submission Unpacker Bash Script.\n"
 printf "This project is available at https://github.com/jacksonporter/canvassubmissionunpacker\n\n"
 
-# Start running tasks in an interactive way with the user. 
+# Process command line arguments.
+if [ $# -eq 0 ]
+then 
+    printf "\nWill run in interactive mode."
+    setGlobalVariables
+else
+    while [ $# -ne 0 ]
+    do
+        num1=$1
+        printf "\nnum1 is: $num1\n"
+
+        if [[ $num1 == -* ]]
+        then
+            num1="${num1#?}"
+            printf "\nnum1 now is: $num1\n"
+
+            while [[ ! -z "$num1" ]]
+            do
+                firstLetter="$(echo $num1 | head -c 1)"
+                printf "firstLetter is: $firstLetter\n"
+                if [[ "$firstLetter" == "u" ]]
+                then
+                    printUsage
+                    cleanUp 0
+                elif [[ "$firstLetter" == "i" ]]
+                then
+                    while [ ! -z $1 ]
+                    do
+                        shift 1
+                    done
+
+                    setGlobalVariables
+
+                    printf "\n***Will run in interactive mode.***\n\n"
+                    num1=""
+                    break
+                elif [[ "$firstLetter" == "s" ]]
+                then
+                    shift 1
+                    COMPRESSEDSUBMISSIONS="$1"
+                    printf "Set compressed zip folder as: $COMPRESSEDSUBMISSIONS\n"
+                    shift 1
+                elif [[ "$firstLetter" == "a" ]]
+                then
+                    shift 1
+                    ASSINGMENTNAME="$1"
+                    printf "Set assingment name as: $ASSINGMENTNAME\n"
+                    shift 1
+                else
+                    printf "\nInvalid argument or missing value. Run bash CanvasSubmissionUnpacker.sh -u for usage."
+                    cleanUp 1
+                fi
+
+                num1=${num1#?}
+                printf "\nnum1 reset in condition is: $num1\n"
+            done
+        elif [[ -z "$num1" ]]
+        then
+            printf "\nNo more args!\n"
+        else
+            printf "\nInvalid argument or missing value. Run bash CanvasSubmissionUnpacker.sh -u for usage."
+            cleanUp 1
+        fi
+    done
+fi
+
+# Start running tasks
 setCompressedZipSubmissions #set the location of submission compressed zip folder
 makeAssingmentDirectory #create a directory for this assingment and more me to work in
 
